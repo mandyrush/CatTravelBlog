13df2a24b7e957b38d141f4ad775194fdc6c5616
Amanda Rush <mandyrush85@gmail.com>
2017-10-16 21:58:14 +0200

    Switch to using controllers and add route model binding

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,16 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Posts;
 
 class PostsController extends Controller
 {
     public function index()
     {
-    	return view('posts.index');
+        $posts = Posts::all();
+        return view('posts.index', compact('posts'));
     }
-    public function show()
+    public function show(Posts $post)
     {
-    	return view('posts.show');
+        return view('posts.show', compact('post'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,4 @@
 <?php
 
-Route::get('/', function () {
-    $posts = DB::table('posts')->get();
-    return view('posts.index', compact('posts'));
-});
-
-Route::get('posts/{id}', function ($id) {
-     $post = DB::table('posts')->find($id);
-     return view('posts.show', compact('post'));
-});
+Route::get('/', 'PostsController@index');
+Route::get('/posts/{post}', 'PostsController@show');
