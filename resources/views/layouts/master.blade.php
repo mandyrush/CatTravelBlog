a8500aa5b0df5955d0d982ead87feb91fce025fa
Amanda Rush <mandyrush85@gmail.com>
2017-12-12 18:25:30 +0100

    clean and restructure sass file

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,18 +3,19 @@
 <head>
 	<meta charset="UTF-8">
 	<title>KittyTravelBlog</title>
+	<!-- Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Cookie|Prociono" rel="stylesheet">
+
 	<!-- CSS -->
-	<link href="/css/app.css" type="text/css" rel="stylesheet">
-	<!-- Bootstrap -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+	<link href="/css/app.css" type="text/css" rel="stylesheet">
+
 	<!-- jQuery -->
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<!-- Required plugin for Bootstrap dropdowns -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
 	<!-- Bootstrap JS -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-	<!-- Fonts -->
-	<link href="https://fonts.googleapis.com/css?family=Cookie|Prociono" rel="stylesheet">
 </head>
 <body>
 	@include ('partials.nav')
