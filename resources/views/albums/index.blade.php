a9d1ecccfc006f49d8be6d44475251de197cbf68
Amanda Rush <mandyrush85@gmail.com>
2018-01-09 16:46:15 +0100

    redesign album index page

--- resources/views/albums/index.blade.php
+++ resources/views/albums/index.blade.php
@@ -1,18 +1,23 @@
 @extends ('layouts.master')
 
 @section ('content')
-	<div class="container-fluid">
+	<div class="container-fluid album-index">
 		<div class="row">
 
 			@foreach($albums as $album)
-				<div class="col-sm-4 album">
+				<div class="col-md-4 album">
 					
 					@foreach($album->tags as $tag)
 						<a href="/images/tags/{{$tag->name}}">
-							<div class="album-tile">
+							
+							<div class="album-content">
 								<img src="{{Storage::disk('local')->url( $album->cover_image )}}" class="img-fluid" alt="Responsive image">
-								{{$album->title}}
+							
+								<div class="album-title">
+									{{$album->title}}
+								</div>
 							</div>
+
 						</a>
 					@endforeach
 			
