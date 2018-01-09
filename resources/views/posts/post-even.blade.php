9477368a1b8b6a18eb02859d29d5c0183b62f663
Amanda Rush <mandyrush85@gmail.com>
2018-01-09 14:05:44 +0100

    Add author to post index and update grid sizing to md

--- resources/views/posts/post-even.blade.php
+++ resources/views/posts/post-even.blade.php
@@ -3,16 +3,17 @@
 <div class="container-fluid">
 	<div class="row posts even justify-content-center">
 
-		<figure class="col-sm-4">
+		<figure class="col-md-4">
 			<a href="/posts/{{ $post->id }}">
 				<img src="{{Storage::disk('local')->url( $post->featured_image )}}" class="img-fluid" alt="Responsive image">
 			</a>
 		</figure>
 
-		<div class="col-sm-6 post-info">
+		<div class="col-md-6 post-info">
 			<h2>
 				<a href="/posts/{{ $post->id }}" class="post-title">{{ $post->title }}</a>
 			</h2>
+			<h6> Written by {{ $post->user->name }} </h6>
 			<h6>{{ $post->created_at->toFormattedDateString() }}</h6>
 			<h5>{{ $post->featured_text }}</h5>
 			<h6>
