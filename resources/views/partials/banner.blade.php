09e059f73b6fefac89710be6d516f346a21ceb74
Amanda Rush <mandyrush85@gmail.com>
2018-01-16 00:40:45 +0100

    try to fix banner logo

--- resources/views/partials/banner.blade.php
+++ resources/views/partials/banner.blade.php
@@ -1,8 +1,10 @@
 <div class="banner">
     <div class="banner-text">
     	<div class="pretty-border">
-			<h3>Cats Wander</h3>
-			<h6>Come with us! As me "Nessie", and my acquaintance Mozzie travel the world with <br> our enslaved humans to the far reaching corners of the globe.</h6>
+    		<div>
+				<h3>Cats Wander</h3>
+				<h6>Come with us! As me "Nessie", and my acquaintance Mozzie travel the world with <br> our enslaved humans to the far reaching corners of the globe.</h6>
+			</div>
 		</div>
     </div>
 </div>
