ccadf31416f5dc4092fb8f3317d4d5f13e420d48
Amanda Rush <mandyrush85@gmail.com>
2018-01-18 23:57:27 +0100

    make photo masonry panels responsive and add flip card for Nessie says

--- resources/views/photos/index.blade.php
+++ resources/views/photos/index.blade.php
@@ -8,11 +8,25 @@
 			<div class="masonry-panel">
 				<div class="masonry-panel_content">
 					<a href="{{Storage::disk('local')->url( $photo->featured_photo )}}" target="_blank">
-						<img src="{{Storage::disk('local')->url( $photo->featured_photo )}}" class="img-fluid" alt="Responsive image">
+						<img class="img-fluid" src="{{Storage::disk('local')->url( $photo->featured_photo )}}" alt="Responsive image">
 					</a>
 				</div>
 			</div>
 		@endforeach
 
+
+		<div class="masonry-panel flip-med flip">
+			<div class="masonry-panel_content flip-content">
+				<div class="flip-panel flip-panel-front">
+					<p>Nessie say...</p>
+				</div>
+				<div class="flip-panel flip-panel-back">
+					<p>A stick in the paw is worth 2 under the fridge.</p>
+				</div>
+			</div>
+		</div>
+
+
 	</div>
+
 @endsection
