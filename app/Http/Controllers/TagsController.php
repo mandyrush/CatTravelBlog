3f700bd3b5d32963f25a1ef7bd1a0c6fc8f1e813
Amanda Rush <mandyrush85@gmail.com>
2018-01-19 23:29:26 +0100

    get correct images and tags to show in modal window

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Tag;
-use Illuminate\Http\Request;
 
 class TagsController extends Controller
 {
@@ -12,6 +11,7 @@ class TagsController extends Controller
         $posts = $tag->posts;
         return view('posts.index', compact('posts'));
     }
+
     public function photos(Tag $tag)
     {
         $photos = $tag->photos;
