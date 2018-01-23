17115fb86f1225728917cd9ad65dc5d8fe910e7b
Amanda Rush <mandyrush85@gmail.com>
2018-01-23 20:36:17 +0100

    remove top padding from home page and fix sizing and border of banner

--- resources/views/partials/banner.blade.php
+++ resources/views/partials/banner.blade.php
@@ -1,8 +1,14 @@
 <div class="banner">
+
+	<div class="home-white-border">
+		<div></div>
+	</div>
+
     <div class="banner-text">
 		<div class="pretty-border">
 			<h3>Cats Wander</h3>
 			<p>Come with us! As me "Nessie", and my acquaintance Mozzie travel the world with our enslaved humans to the far reaching corners of the globe.</p>
 		</div>
     </div>
+
 </div>
