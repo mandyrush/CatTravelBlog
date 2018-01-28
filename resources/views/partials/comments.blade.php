bc09d206ef689f36a567be754f0e173d7e84fdbc
Amanda Rush <mandyrush85@gmail.com>
2018-01-28 17:33:42 +0100

    move comments to a partial

---
+++ resources/views/partials/comments.blade.php
@@ -0,0 +1,44 @@
+{{-- Posted Comments --}}
+<div class="col-sm-5 comments">
+
+    <div class="user-comments">
+        <ul class="list-group">
+            @foreach($post->comments as $comment)
+                <li class="list-group-item">
+                    <strong>
+                        {{ $comment->user->name }}: &nbsp;
+                    </strong>
+                    <strong>
+                        {{ $comment->created_at->DiffForHumans() }} &nbsp;
+                    </strong>
+                    <p>
+                        {{ $comment->body }}
+                    </p>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+
+    {{-- Add a comment --}}
+    <hr>
+    @if (Auth::check())
+        <form method="POST" action="/posts/{{ $post->id }}/comments">
+            {{ csrf_field() }}
+
+            <div class="form-group">
+                <textarea name="body" placeholder="Your comment here" class="form-control" required></textarea>
+            </div>
+
+            <div class="form-group">
+                <button type="submit" class="btn btn-secondary btn-lg">Add Comment</button>
+            </div>
+        </form>
+    @else
+        <div class="form-group">
+            <a href="/login">
+                <button class="btn btn-secondary btn-lg">Login a to add a comment</button>
+            </a>
+        </div>
+    @endif
+
+</div>
