1bad0c3a63b1673d462966f753bdeb8fbdd14278
Amanda Rush <mandyrush85@gmail.com>
2018-01-29 22:45:58 +0100

    wip - make banner and banner text responsive

--- resources/views/partials/banner.blade.php
+++ resources/views/partials/banner.blade.php
@@ -1,14 +1,18 @@
-<div class="banner">
+<div class="container-fluid">
+	<div class="row">
 
-	<div class="home-white-border">
-		<div></div>
-	</div>
+		<div class="banner col-md-12">
 
-    <div class="banner-text">
-		<div class="pretty-border">
-			<h3>Cats Wander</h3>
-			<p>Come with us! As me "Nessie", and my acquaintance Mozzie travel the world with our enslaved humans to the far reaching corners of the globe.</p>
+			<div class="home-white-border"></div>
+
+			<div class="banner-text col-md-4">
+				<div class="pretty-border col-md-12">
+					<h3>Cats Wander</h3>
+					<p>Come with us! As me "Nessie", and my acquaintance Mozzie travel the world with our enslaved humans to the far reaching corners of the globe.</p>
+				</div>
+			</div>
+
 		</div>
-    </div>
 
+	</div>
 </div>
