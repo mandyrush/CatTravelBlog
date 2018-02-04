4867723306ab5d288087e8c9337880dcc541ffda
Desmond Rush <blueyama@gmail.com>
2018-02-04 22:56:42 +0100

    Attention Update DB! check "posts" table scheme

--- database/database_schema.sql
+++ database/database_schema.sql
@@ -131,6 +131,7 @@ CREATE TABLE `posts` (
   `featured_image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
+  `status` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
