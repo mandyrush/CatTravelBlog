2ce09d40fd8a5f1507ee16639c1cb5009a5061d2
Desmond Rush <blueyama@gmail.com>
2018-04-09 08:03:32 +0100

    Updated admin pic and display name

--- resources/views/admin/layouts/partials/left_sidebar.blade.php
+++ resources/views/admin/layouts/partials/left_sidebar.blade.php
@@ -7,10 +7,10 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel">
             <div class="pull-left image">
-                <img src="{{asset('vendor/adminlte/dist/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image">
+                <img src="{{asset('vendor/adminlte/dist/img/user2-160x160.png')}}" class="img-circle" alt="User Image">
             </div>
             <div class="pull-left info">
-                <p>Alexander Pierce</p>
+                <p>{{ Auth::user()->name }}</p>
                 <!-- Status -->
                 <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
